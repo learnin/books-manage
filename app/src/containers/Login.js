d69ac56711a1213420ad71f47d1ab5bcddb81dcf
learnin <i.learnin@gmail.com>
2017-09-11 23:33:09 +0900

    Refactor

--- app/src/containers/Login.js
+++ app/src/containers/Login.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 import * as Actions from '../actions';
 
 class LoginContainer extends React.Component {
+  static propTypes = {
+    actions: PropTypes.object.isRequired
+  };
+
   render() {
     const { actions } = this.props;
 
@@ -13,22 +18,21 @@ class LoginContainer extends React.Component {
       <div>
         <input type="text" ref="username" id="username" /><br/>
         <input type="password" ref="password" id="password" /><br/>
-        <button onClick={() => actions.fetchLoginIfNeeded(ReactDOM.findDOMNode(this.refs.username).value.trim(), ReactDOM.findDOMNode(this.refs.password).value.trim())}>ログイン</button>
+        <button onClick={() => actions.fetchLoginIfNeeded(
+          ReactDOM.findDOMNode(this.refs.username).value.trim(),
+          ReactDOM.findDOMNode(this.refs.password).value.trim()
+        )}>ログイン</button>
       </div>
     );
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(Actions, dispatch)
-  };
-}
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(Actions, dispatch)
+});
 
-function mapStateToProps(state) {
-  return {
-    isLogined: state.authenticator.isLogined
-  };
-}
+const mapStateToProps = state => ({
+  isLogined: state.authenticator.isLogined
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer);
