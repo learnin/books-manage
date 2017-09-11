51f5424b8e2bb19aeaeefd5199018d5d12e48b35
learnin <i.learnin@gmail.com>
2017-09-11 22:27:20 +0900

    Refactor

--- app/src/containers/Login.js
+++ app/src/containers/Login.js
@@ -1,23 +1,34 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import * as actions from '../actions';
+import * as Actions from '../actions';
 
-const handleSubmit = (e, dispatch) => {
-  e.preventDefault();
-  dispatch(actions.fetchLoginIfNeeded(e.target.username.value.trim(), e.target.password.value.trim()));
-};
+class LoginContainer extends React.Component {
+  render() {
+    const { actions } = this.props;
 
-const LoginContainer = ({ dispatch }) => {
-  return (
-    <div>
-      <form onSubmit={(e) => handleSubmit(e, dispatch)}>
-        <input type="text" name="username" /><br/>
-        <input type="password" name="password" /><br/>
-        <input type="submit" value="ログイン" />
-      </form>
-    </div>
-  );
-};
+    return (
+      <div>
+        <input type="text" ref="username" id="username" /><br/>
+        <input type="password" ref="password" id="password" /><br/>
+        <button onClick={() => actions.fetchLoginIfNeeded(ReactDOM.findDOMNode(this.refs.username).value.trim(), ReactDOM.findDOMNode(this.refs.password).value.trim())}>ログイン</button>
+      </div>
+    );
+  }
+}
 
-export default connect()(LoginContainer);
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(Actions, dispatch)
+  };
+}
+
+function mapStateToProps(state) {
+  return {
+    isLogined: state.authenticator.isLogined
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginContainer);
